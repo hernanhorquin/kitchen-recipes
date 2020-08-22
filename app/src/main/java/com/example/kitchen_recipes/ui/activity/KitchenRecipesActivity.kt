7d33c3723961f86ca54077540f73a59ccd2db5e4
hernanhorquin96 <hernan.horquin@globant.com>
2020-08-22 15:11:58 -0300

    Project structure + add dependencies

---
+++ app/src/main/java/com/example/kitchen_recipes/ui/activity/KitchenRecipesActivity.kt
@@ -0,0 +1,12 @@
+package com.example.kitchen_recipes.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.kitchen_recipes.R
+
+class KitchenRecipesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kitchen_recipes)
+    }
+}
